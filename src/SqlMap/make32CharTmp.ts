e6d0f5909021c83220e1fe9efda75bc587fa5f80
Crypt00o <octopus2672001@gmail.com>
2022-09-09 01:57:23 +0200

    Update Tool

---
+++ src/SqlMap/make32CharTmp.ts
@@ -0,0 +1,10 @@
+function make32CharTmp():string {
+    let result           = [];
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < 32; i++ ) {
+      result.push(characters.charAt(Math.floor(Math.random() * 
+ charactersLength)));
+   }
+   return result.join('');
+}
--- src/SqlMap/sqlParser.ts
+++ src/SqlMap/sqlParser.ts
@@ -1,12 +1,16 @@
 import { sqlFilterAndPrepare } from "./sqlFilterAndPrepare";
+import { make32CharTmp } from "./make32CharTmp";
 const sqlParser=(line:string,args:Array<any>):string=>{
+    const tmp=make32CharTmp()
     for (let i=args.length;i>0;--i){
-         line=line.replace(RegExp(`\\$${i}`,'g'),sqlFilterAndPrepare( args[i-1]))
+         line=line.replace(RegExp(`\\$${i}`,'g'),sqlFilterAndPrepare( args[i-1].replace('$',tmp)))
      }
+     line=line.replace(tmp,'$')
      return line;
+
     
 
     
 
 }
-export{sqlParser}
+export {sqlParser}
